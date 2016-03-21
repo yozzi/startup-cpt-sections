84d578d80ad3c6a7b434349f9946e4fa5fa4d117
yozz <yozz.online@gmail.com>
2016-03-21 09:27:30 -0400

    Minor

--- startup-cpt-sections.php
+++ startup-cpt-sections.php
@@ -327,7 +327,7 @@ add_shortcode( 'section', 'startup_cpt_sections_shortcode' );
 
 // Shortcode UI
 /**
- * Detecion de Shortcake. Identique dans tous les plugins.
+ * Detection de Shortcake. Identique dans tous les plugins.
  */
 if ( !function_exists( 'shortcode_ui_detection' ) ) {
     function shortcode_ui_detection() {
